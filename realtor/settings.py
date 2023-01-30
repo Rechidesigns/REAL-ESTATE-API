ee0ec7196c5b300580b33ed07f4000d5ce3b7f2b
Alerechi Ordu <alerechiordu@gmail.com>
2023-01-30 17:05:36 +0100

    Update settings.py
    add to the local host's

--- realtor/settings.py
+++ realtor/settings.py
@@ -30,7 +30,7 @@ API_SECRET = os.getenv('CLOUD_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", '127.0.0.1']
 
 # Application definition
 
